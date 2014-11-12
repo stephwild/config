3af194e4c81ca68fecd8aa3acf4c0c4ddd3c49d6
DELGADO Stephane <stephane.delgado@epita.fr>
2014-11-12 09:51:30 +0100

    add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+# .vimrc config file
+echo "Add vimrc in $HOME"
+cp vimrc ~/.vimrc
+echo "Done vimrc"
+
+# .emacs config file
+echo "Add vimrc in $HOME"
+cp emacs ~/.emacs
+echo "Done vimrc"
+
+# .bashrc config file
+echo "Add resources in $HOME"
+cp bashrc ~/.bashrc
+echo "Done bashrc"
+
+# .Xresources config file
+echo "Add Xresources in $HOME"
+cp Xresources ~/.Xresources
+echo "Done Xresources"
