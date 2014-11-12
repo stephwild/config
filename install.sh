8555405ddad923be14b2d96250f177fbc39f3ee9
DELGADO Stephane <stephane.delgado@epita.fr>
2014-11-12 09:59:20 +0100

    add verbose output to install script

--- install.sh
+++ install.sh
@@ -1,19 +1,21 @@
 # .vimrc config file
 echo "Add vimrc in $HOME"
-cp vimrc ~/.vimrc
-echo "Done vimrc"
+cp -v vimrc ~/.vimrc
+echo -e "Done vimrc\n"
 
 # .emacs config file
 echo "Add vimrc in $HOME"
-cp emacs ~/.emacs
-echo "Done vimrc"
+cp -v emacs ~/.emacs
+echo -e "Done vimrc\n"
 
 # .bashrc config file
 echo "Add resources in $HOME"
-cp bashrc ~/.bashrc
-echo "Done bashrc"
+cp -v bashrc ~/.bashrc
+echo -e "Done bashrc\n"
 
 # .Xresources config file
 echo "Add Xresources in $HOME"
-cp Xresources ~/.Xresources
+cp -v Xresources ~/.Xresources
+echo "Load new Xresources"
+xrdb ~/.Xresources
 echo "Done Xresources"
