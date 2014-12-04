edfd97c839e7f9326a10c4592eb4d23c7e995713
DELGADO Stephane <stephane.delgado@epita.fr>
2014-12-04 02:25:36 +0100

    Remove some unused comment in ycm file

--- vim/ycm_extra_conf.py
+++ vim/ycm_extra_conf.py
@@ -4,32 +4,18 @@ import ycm_core
 
 # These are the compilation flags that will be used in case there's no
 # compilation database set (by default, one is not set).
-# CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
+# MUST put a -std flag (YCM so know which compilator use)
+# SHOULD '-x' flag -> file where we work in (for header)
 flags = [
     '-Wall',
     '-Wextra',
-    # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
-    # language to use when compiling headers. So it will guess. Badly. So C++
-    # headers will be compiled as C headers. You don't want that so ALWAYS specify
-    # a "-std=<something>".
-    # For a C project, you would set this to something like 'c99' instead of
-    # 'c++11'.
     '-std=c99',
-    # ...and the same thing goes for the magic -x option which specifies the
-    # language that the files to be compiled are written in. This is mostly
-    # relevant for c++ headers.
-    # For a C project, you would set this to 'c' instead of 'c++'.
     '-x', 'c',
-    # This path will only work on OS X, but extra paths that don't exist are not
-    # harmful
 ]
 
 # Set this to the absolute path to the folder (NOT the file!) containing the
 # compile_commands.json file to use that instead of 'flags'. See here for
 # more details: http://clang.llvm.org/docs/JSONCompilationDatabase.html
-#
-# Most projects will NOT need to set this to anything; you can just change the
-# 'flags' list of compilation flags. Notice that YCM itself uses that approach.
 compilation_database_folder = ''
 
 if compilation_database_folder:
